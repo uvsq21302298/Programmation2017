cd342d4f9c76a015581d05f76c740114866dc805
moumzy <uvsq21302298>
2017-10-04 13:38:27 +0200

    test

---
+++ exo6-1d/src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class Main
+     */
+	public static void main(String[] args){
+        System.out.println("test compilation");
+	}
+        
+    }
+
